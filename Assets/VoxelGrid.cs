41a7f0c0c9a4996054c49f3d2f18a66b33dc1c9d
Stefan Andersson <stf.andersson@gmail.com>
2021-06-08 13:09:49 +0200

    Revert "Got a purely visual voxelmap working, using the octree"
    This reverts commit 78ae277fa7cec3d171336cb28418cf0cb8f5fcb4.

--- Assets/VoxelGrid.cs
+++ Assets/VoxelGrid.cs
@@ -15,8 +15,7 @@ public partial class VoxelGrid : MonoBehaviour
 
     private Vector3Int binGridDimensions;
     private Bin[] bins;
-    private Octree<bool> voxelMap;
-
+    
     private const float UPDATE_LATENCY = 0.1f;
     private float timeToUpdate;
     private Queue<int> dirtyBins = new Queue<int>();
@@ -33,7 +32,7 @@ public partial class VoxelGrid : MonoBehaviour
 
     private void Start() {
         if(isOriginal) {
-            Vector3Int voxelGridDimensions = new Vector3Int(4, 8, 4);
+            Vector3Int voxelGridDimensions = new Vector3Int(32, 64, 32);
             int voxelCount = voxelGridDimensions.x * voxelGridDimensions.y * voxelGridDimensions.z;
 
             binGridDimensions = voxelGridDimensions / Bin.WIDTH;
@@ -43,8 +42,6 @@ public partial class VoxelGrid : MonoBehaviour
             for(int i = 0; i < binCount; i++) {
                 bins[i] = new Bin(i, binGridDimensions);
             }
-
-            voxelMap = new Octree<bool>(Mathf.Max(voxelGridDimensions.x, Mathf.Max(voxelGridDimensions.y, voxelGridDimensions.z)));
 
             int minX = Bin.WIDTH - 1;
             int minY = Bin.WIDTH - 1;
@@ -56,9 +53,8 @@ public partial class VoxelGrid : MonoBehaviour
 
             for(int i = 0; i < voxelCount; i++) {
                 Vector3Int voxelCoords = IndexToCoords(i, voxelGridDimensions);
-                voxelMap.SetValue(voxelCoords.x, voxelCoords.y, voxelCoords.z, true);
-                
                 Vector3Int binCoords = voxelCoords / Bin.WIDTH;
+                
                 int binIndex = CoordsToIndex(binCoords, binGridDimensions);
 
                 if(voxelCoords.x > minX && voxelCoords.y > minY && voxelCoords.z > minZ && voxelCoords.x < maxX && voxelCoords.y < maxY && voxelCoords.z < maxZ) {
@@ -87,27 +83,7 @@ public partial class VoxelGrid : MonoBehaviour
             // this just ensures that the initial building will be in the same spot as it was placed in the editor - a bit ugly, but I haven't figured out anything better yet
             meshTransform.localPosition = new Vector3(-(voxelGridDimensions.x / 2f), 0.5f, -(voxelGridDimensions.z / 2f));
 
-            ApplyCluster(new VoxelCluster(bins, voxelMap, Vector3Int.zero, binGridDimensions));
-        }
-    }
-
-    float timeToUpdateDrawing;
-    private void Update() {
-        if(timeToUpdateDrawing - Time.time > 0f) {
-            return;
-        }
-
-        timeToUpdateDrawing = Time.time + UPDATE_LATENCY;
-        DebugDrawVoxelMap();
-    }
-
-    private void DebugDrawVoxelMap() {
-        for(int z = 0; z < voxelMap.Size; z++) {
-            for(int y = 0; y < voxelMap.Size; y++) {
-                for(int x = 0; x < voxelMap.Size; x++) {
-                    voxelMap.TryGetValue(x, y, z, out bool value, DebugDrawNode);
-                }
-            }
+            ApplyCluster(new VoxelCluster(bins, Vector3Int.zero, binGridDimensions));
         }
     }
 
@@ -144,17 +120,21 @@ public partial class VoxelGrid : MonoBehaviour
             return (t.TransformPoint(localPos) - t.position);
         }
 
-        meshTransform.position += GetLocalPosWithWorldRotation(voxelCluster.VoxelOffset, meshTransform);
-        Vector3 cachedMeshTransformPos = meshTransform.position;
-        
         Vector3 pivot = GetPivot(voxelCluster.Bins, voxelCluster.Dimensions, isStatic);
+
+        //meshTransform.position += GetLocalPosWithWorldRotation(voxelCluster.VoxelOffset, meshTransform);
+        //meshTransform.parent = null;
+        //transform.position = meshTransform.position + GetLocalPosWithWorldRotation(pivot, meshTransform);
+        //meshTransform.parent = transform;
+
+        meshTransform.position += GetLocalPosWithWorldRotation(voxelCluster.VoxelOffset, meshTransform);
+        
+        Vector3 cachedMeshTransformPos = meshTransform.position;
         transform.position = meshTransform.position + GetLocalPosWithWorldRotation(pivot, meshTransform);
-       
         meshTransform.position = cachedMeshTransformPos;
 
         binGridDimensions = voxelCluster.Dimensions;
         bins = voxelCluster.Bins;
-        voxelMap = voxelCluster.VoxelMap;
 
         voxelBuilder.Refresh();
     }
@@ -179,10 +159,6 @@ public partial class VoxelGrid : MonoBehaviour
         return bins.Length;
     }
 
-    public Octree<bool> GetVoxelMap() {
-        return voxelMap;
-    }
-
     public Vector3Int GetBinGridDimensions() {
         return binGridDimensions;
     }
@@ -204,7 +180,6 @@ public partial class VoxelGrid : MonoBehaviour
     }
 
     public void TrySetVoxelExists(Vector3Int voxelCoords, bool exists) {
-        voxelMap.SetValue(voxelCoords.x, voxelCoords.y, voxelCoords.z, exists);
 
         int binIndex, localVoxelIndex;
         if(!GetBinAndVoxelIndex(voxelCoords, bins, binGridDimensions, out binIndex, out localVoxelIndex)) {
@@ -298,63 +273,5 @@ public partial class VoxelGrid : MonoBehaviour
             Mathf.FloorToInt(targetLocalPos.y + 0.5f), 
             Mathf.FloorToInt(targetLocalPos.z + 0.5f)
         );
-    }
-
-    private void DebugDrawNode(int nodeOffsetX, int nodeOffsetY, int nodeOffsetZ, int nodeSize, int gridSize, bool value) {
-        Random.seed = nodeOffsetX + gridSize * (nodeOffsetY + gridSize * nodeOffsetZ);
-
-        float halfSize = nodeSize * 0.5f;
-        Vector3 drawPos = new Vector3(nodeOffsetX + halfSize, nodeOffsetY + halfSize, nodeOffsetZ + halfSize);
-
-        float s = halfSize * 0.99f;
-
-        Vector3 left = Vector3.left;
-        Vector3 right = Vector3.right;
-        Vector3 up = Vector3.up;
-        Vector3 down = Vector3.down;
-        Vector3 fore = Vector3.forward;
-        Vector3 back = Vector3.back;
-
-        Vector3 bottomLeftWorldPos = new Vector3(-0.5f, -0.5f, -0.5f);
-        Vector3 leftDownBack =      meshTransform.TransformPoint(bottomLeftWorldPos + drawPos + (left + down + back)  * s);
-        Vector3 rightDownBack =     meshTransform.TransformPoint(bottomLeftWorldPos + drawPos + (right + down + back) * s);
-        Vector3 leftUpBack =        meshTransform.TransformPoint(bottomLeftWorldPos + drawPos + (left + up + back)    * s);
-        Vector3 rightUpBack =       meshTransform.TransformPoint(bottomLeftWorldPos + drawPos + (right + up + back)   * s);
-        Vector3 leftDownForward =   meshTransform.TransformPoint(bottomLeftWorldPos + drawPos + (left + down + fore)  * s);
-        Vector3 rightDownForward =  meshTransform.TransformPoint(bottomLeftWorldPos + drawPos + (right + down + fore) * s);
-        Vector3 leftUpForward =     meshTransform.TransformPoint(bottomLeftWorldPos + drawPos + (left + up + fore)    * s);
-        Vector3 rightUpForward =    meshTransform.TransformPoint(bottomLeftWorldPos + drawPos + (right + up + fore)   * s);
-
-        Color color = value ? Color.red : Color.clear;
-
-        Debug.DrawLine(leftDownBack,        leftUpBack,         color, UPDATE_LATENCY);
-        Debug.DrawLine(leftUpBack,          rightUpBack,        color, UPDATE_LATENCY);
-        Debug.DrawLine(rightUpBack,         rightDownBack,      color, UPDATE_LATENCY);
-        Debug.DrawLine(rightDownBack,       leftDownBack,       color, UPDATE_LATENCY);
-
-        Debug.DrawLine(leftDownForward,     leftUpForward,      color, UPDATE_LATENCY);
-        Debug.DrawLine(leftUpForward,       rightUpForward,     color, UPDATE_LATENCY);
-        Debug.DrawLine(rightUpForward,      rightDownForward,   color, UPDATE_LATENCY);
-        Debug.DrawLine(rightDownForward,    leftDownForward,    color, UPDATE_LATENCY);
-
-        Debug.DrawLine(leftDownBack,        leftDownForward,    color, UPDATE_LATENCY);
-        Debug.DrawLine(leftDownForward,     leftUpForward,      color, UPDATE_LATENCY);
-        Debug.DrawLine(leftUpForward,       leftUpBack,         color, UPDATE_LATENCY);
-        Debug.DrawLine(leftUpBack,          leftDownBack,       color, UPDATE_LATENCY);
-
-        Debug.DrawLine(rightDownBack,       rightDownForward,   color, UPDATE_LATENCY);
-        Debug.DrawLine(rightDownForward,    rightUpForward,     color, UPDATE_LATENCY);
-        Debug.DrawLine(rightUpForward,      rightUpBack,        color, UPDATE_LATENCY);
-        Debug.DrawLine(rightUpBack,         rightDownBack,      color, UPDATE_LATENCY);
-
-        Debug.DrawLine(leftUpBack,          leftUpForward,      color, UPDATE_LATENCY);
-        Debug.DrawLine(leftUpForward,       rightUpForward,     color, UPDATE_LATENCY);
-        Debug.DrawLine(rightUpForward,      rightUpBack,        color, UPDATE_LATENCY);
-        Debug.DrawLine(rightUpBack,         leftUpBack,         color, UPDATE_LATENCY);
-
-        Debug.DrawLine(leftDownBack,        leftDownForward,    color, UPDATE_LATENCY);
-        Debug.DrawLine(leftDownForward,     rightDownForward,   color, UPDATE_LATENCY);
-        Debug.DrawLine(rightDownForward,    rightDownBack,      color, UPDATE_LATENCY);
-        Debug.DrawLine(rightDownBack,       leftDownBack,       color, UPDATE_LATENCY);
     }
 }
